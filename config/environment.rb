cf2c90f3ccfa29102ae803de3df4b33a6fb55bb3
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-03-21 20:47:37 +0530

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Darshan::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Darshan::Application.config.secret_key_base = '76b879530c36de1573a7efde2a29596f5fae9cbff43aa0b8b2f79de0f20b7388f33f76c3a30ad352ffe1e77f47b6efa44751ab5d7e98f710d5a548295882a305'
